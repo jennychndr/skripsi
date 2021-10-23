5d110c4d850a4fba0b5f24ebd8398ac387d2a541
jennychndr <j.chdr61@gmail.com>
2021-10-24 05:26:08 +0700

    front end

---
+++ pages/detailFandom.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-card>
+    <v-toolbar
+
+      dark
+      flat
+    >
+       <v-btn icon>
+        <v-icon>mdi-arrow-left</v-icon>
+      </v-btn>
+
+      <v-toolbar-title>BTS</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+
+
+
+
+      <template v-slot:extension>
+        <v-tabs
+          v-model="tab"
+          align-with-title
+        >
+          <v-tabs-slider color="yellow"></v-tabs-slider>
+
+          <v-tab
+            v-for="item in items"
+            :key="item"
+          >
+            {{ item }}
+          </v-tab>
+        </v-tabs>
+      </template>
+    </v-toolbar>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item
+        v-for="item in items"
+        :key="item"
+      >
+        <v-card flat>
+          <v-card-text v-text="text"></v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
+</template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        tab: null,
+        items: [
+          'Live', 'Podcast', 'News', 'Products', 'news','Group Chat', 'Concert', 'Event',
+        ],
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      }
+    },
+  }
+</script>
---
+++ pages/profile.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-card
+    class="mx-auto"
+    height=20%
+    tile
+  >
+    <v-img
+      height="250px"
+      src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
+    >
+      <v-row
+        align="end"
+
+      >
+        <v-col
+          align-self="start"
+          class="pa-0"
+          cols="12"
+        >
+          <v-avatar
+            class="profile"
+            color="grey"
+            size="164"
+            tile
+          >
+            <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
+          </v-avatar>
+        </v-col>
+        <v-col class="py-0">
+          <v-list-item
+            color="rgba(0, 0, 0, .4)"
+            dark
+          >
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+                Marcus Obrien
+              </v-list-item-title>
+
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
+    </v-img>
+  </v-card>
+</template>
